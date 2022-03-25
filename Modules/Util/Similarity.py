c756e55d4b9a5af585c1492f364387a02e87647b
pepsizerosugar <juntiger53@gmail.com>
2022-03-25 17:54:32 +0900

    [pepsi-04] REFACTOR: change calculate similarity logic

--- Modules/Util/Similarity.py
+++ Modules/Util/Similarity.py
@@ -1,62 +1,65 @@
+import PrintHandler
 from Modules.Interface.Dialog import Dialogs
-from Modules.Util.DataClass.Target import Targets
+from Modules.Util.DataClass.Targets import Targets
+
+Targets.difference_element_list = []
 
 
-def calculate_similarity(target, comp_target):
-    Targets.score = calculate_final_score(get_similarity_score(target, comp_target))
+def run(target, comp_target):
+    Targets.score = calculate_final_score(calculate_similarity(target, comp_target))
     Dialogs.when_calculate_complete(Targets.score)
 
 
-def get_similarity_score(target, comp_target, weight=None):
+def calculate_similarity(target, comp_target, weight=None):
     score = 0
-    weight = get_key_weight(target.keys()) if weight is None else weight / len(target.keys())
-    then_weight = 0  # for print each depth
+    weight = get_key_weight(target.keys(), weight)
 
     for key in target:
-        # print each depth weight to console
-        if weight == then_weight:
-            print(f"[INFO] key: {key} weight: {weight}")
-        else:
-            print(f"\n[INFO] key: {key} weight: {weight}")
-            then_weight = weight
+        PrintHandler.info(key, weight)
 
         if key not in comp_target:
-            print(f"[WARN] {key} is not in comparison target with weight: {weight}")
+            Targets.difference_element_list.append([key, weight])
+            PrintHandler.when_key_is_not_in_comparison_target(key, weight)
             score += weight
         else:
-            if isinstance(target[key], dict):
-                if isinstance(comp_target[key], dict):
-                    score += get_similarity_score(target[key], comp_target[key], weight)
-                elif isinstance(comp_target[key], list):
-                    for item in comp_target[key]:
-                        score += get_similarity_score(item, target[key], weight)
+            t_value = target[key]
+            ct_value = comp_target[key]
+
+            if isinstance(t_value, dict) and isinstance(ct_value, dict):
+                score += calculate_similarity(t_value, ct_value, weight)
+                continue
+
+            if isinstance(t_value, dict) and isinstance(ct_value, list):
+                for c_item in ct_value:
+                    score += calculate_similarity(c_item, t_value, weight)
+                continue
+
+            if isinstance(t_value, list) and isinstance(ct_value, list):
+                if len(t_value) > len(ct_value):
+                    for i in range(len(ct_value)):
+                        score += calculate_similarity(t_value[i], ct_value[i], weight)
+                    continue
                 else:
-                    score += weight
-            elif isinstance(target[key], list):
-                if isinstance(comp_target[key], list):
-                    if len(target[key]) > len(comp_target[key]):
-                        for i in range(len(comp_target[key])):
-                            score += get_similarity_score(target[key][i], comp_target[key][i], weight)
-                    else:
-                        for i in range(len(target[key])):
-                            score += get_similarity_score(target[key][i], comp_target[key][i], weight)
-                elif isinstance(comp_target[key], dict):
-                    for item in target[key]:
-                        score += get_similarity_score(item, comp_target[key], weight)
-                else:
-                    score += weight
-            else:
-                if target[key] != comp_target[key]:
-                    print(
-                        f"[WARN] key: {key} value: {target[key]} is not equal to {comp_target[key]} with weight: {weight}")
-                    score += weight
+                    for i in range(len(t_value)):
+                        score += calculate_similarity(t_value[i], ct_value[i], weight)
+                    continue
+
+            if isinstance(t_value, list) and isinstance(ct_value, dict):
+                for v_item in t_value:
+                    score += calculate_similarity(v_item, ct_value, weight)
+                continue
+
+            if t_value != ct_value:
+                Targets.difference_element_list.append([key, weight, t_value, ct_value])
+                PrintHandler.when_key_is_not_equal_to_comparison_target(key, weight, t_value, ct_value)
+                score += weight
     return score
 
 
-def get_key_weight(keys):
-    return 1 / len(keys)
+def get_key_weight(keys, weight):
+    return 1 / len(keys) if weight is None else weight / len(keys)
 
 
 def calculate_final_score(num):
-    print(f"\n[INFO] final score: {1 - num}")
+    PrintHandler.final_score(num)
     return round((1 - num) * 100, 6)
