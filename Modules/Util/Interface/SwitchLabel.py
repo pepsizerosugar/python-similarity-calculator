019efcf33a9fbf7bd7044feb82b8bb7e209e7e7b
pepsizerosugar <juntiger53@gmail.com>
2022-03-28 16:02:56 +0900

    [pepsi-16] FEAT: add function to switch each target

---
+++ Modules/Util/Interface/SwitchLabel.py
@@ -0,0 +1,19 @@
+from Modules.Handler.ButtonHandler import ButtonHandler
+from Modules.Interface.DataClass.UIElement import UIElements
+
+
+def switch_label():
+    t_label = UIElements.data["target"]["target_label"].text()
+    ct_label = UIElements.data["comp_target"]["comp_target_label"].text()
+    UIElements.data["target"]["target_label"].setText(ct_label)
+    UIElements.data["comp_target"]["comp_target_label"].setText(t_label)
+
+    t_code_label = UIElements.data["target"]["target_code_label"].toPlainText()
+    ct_code_label = UIElements.data["comp_target"]["comp_target_code_label"].toPlainText()
+    UIElements.data["target"]["target_code_label"].setText(ct_code_label)
+    UIElements.data["comp_target"]["comp_target_code_label"].setText(t_code_label)
+
+    if ButtonHandler.t_loaded and ButtonHandler.ct_loaded:
+        from Modules.Util.DataClass.Targets import Targets
+        Targets.data["target_json"], Targets.data["comp_target_json"] = \
+            Targets.data["comp_target_json"], Targets.data["target_json"]
